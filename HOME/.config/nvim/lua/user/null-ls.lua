e11e56544ef72a5317231a04b04359bde1fb64fd
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2023-05-17 21:00:46 +0000

    Update null-ls cspell diagnostic severity

--- HOME/.config/nvim/lua/user/null-ls.lua
+++ HOME/.config/nvim/lua/user/null-ls.lua
@@ -4,6 +4,9 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.cspell.with({
       extra_args = { "--config", "~/.config/cspell/cspell.json" },
+      diagnostics_postprocess = function(diagnostic)
+        diagnostic.severity = vim.diagnostic.severity.WARN
+      end,
     }),
     null_ls.builtins.code_actions.cspell.with({
       extra_args = { "--config", "~/.config/cspell/cspell.json" },
