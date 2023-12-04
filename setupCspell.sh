f255d664077fbdd814dbedf089c54c3a0d3bd145
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2023-12-04 00:12:46 -0700

    tidy: remove unused configs Neovim and CSpell

--- setupCspell.sh
+++
@@ -1,3 +0,0 @@
-# Overwrite the current cspell config
-mkdir -p ~/.config/cspell
-cp ./HOME/.config/cspell/* ~/.config/cspell/
--- setupNvim.sh
+++
@@ -1,7 +0,0 @@
-# Overwrite the current system Neovim config
-
-rm -rf ~/.config/nvim/
-rm -rf ~/.local/share/nvim/
-
-mkdir -p ~/.config/nvim/
-cp ./HOME/.config/nvim/init.lua ~/.config/nvim/
