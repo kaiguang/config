fca08ed489c326f2594e1bee2e070c5a45a3692a
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2024-04-11 21:39:57 -0600

    karabiner: add mouse keys to tab switching

---
+++ install-ke.sh
@@ -0,0 +1,5 @@
+#!sh
+# Install the config file for Karabiner Elements
+# Excute this file by running `./install-ke.sh` in the root dir of the project
+echo 'Installing config file to ~/.config/karabiner/assets/complex_modifications/kai.json'
+cp ./HOME/.config/karabiner/assets/complex_modifications/kai.json ~/.config/karabiner/assets/complex_modifications/kai.json
